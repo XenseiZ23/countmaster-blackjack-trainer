f7174dc7a30c73336a1b66a5e4c1911eda09ef38
XenseiZ23 <erwin3467@gmail.com>
2026-05-11 15:41:02 -0500

    Integrated Vercel Speed Insights for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import { inject } from "@vercel/analytics";
 
 inject();
