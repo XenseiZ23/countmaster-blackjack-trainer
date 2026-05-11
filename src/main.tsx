8a1b8e3bea9c9ac87fac7260159dc1f8dbe371b2
XenseiZ23 <erwin3467@gmail.com>
2026-05-11 15:43:49 -0500

    Fixed Vercel Speed Insights integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,16 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { inject } from "@vercel/analytics";
 
 inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+      <SpeedInsights />
+    </>
   </StrictMode>,
 );
