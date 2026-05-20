912aa9bf55b7fd3f19053eda6c19be764b8d586d
XenseiZ23 <erwin3467@gmail.com>
2026-05-20 15:59:48 -0500

    Fix About page routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App.tsx';
+import About from './pages/About.tsx';
+
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+createRoot(document.getElementById('root')!).render( <StrictMode> <BrowserRouter> <Routes>
+<Route path="/" element={<App />} />
+<Route path="/about" element={<About />} /> </Routes> </BrowserRouter> </StrictMode>,
 );
